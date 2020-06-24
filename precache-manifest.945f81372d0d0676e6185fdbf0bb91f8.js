be93078349157800c464a25a52b7a5011e843acd
karthikreddybethi <karthikreddybethi@gmail.com>
2020-06-25 01:17:41 +0200

    Updates

---
+++ precache-manifest.945f81372d0d0676e6185fdbf0bb91f8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed91135c10c291f0a122513f6252093",
+    "url": "/mobile-store/index.html"
+  },
+  {
+    "revision": "a5a86069958164e43667",
+    "url": "/mobile-store/static/css/2.787549d6.chunk.css"
+  },
+  {
+    "revision": "ff3164aef41ecc5a2a8e",
+    "url": "/mobile-store/static/css/main.a64b644d.chunk.css"
+  },
+  {
+    "revision": "a5a86069958164e43667",
+    "url": "/mobile-store/static/js/2.7485a475.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/mobile-store/static/js/2.7485a475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3164aef41ecc5a2a8e",
+    "url": "/mobile-store/static/js/main.2d0cbd00.chunk.js"
+  },
+  {
+    "revision": "b5d7427e9465207e7dc0",
+    "url": "/mobile-store/static/js/runtime-main.c9768f79.js"
+  },
+  {
+    "revision": "7ab18e8111b332c6b22d4b85de62f0d2",
+    "url": "/mobile-store/static/media/Ikona1.7ab18e81.jpg"
+  },
+  {
+    "revision": "8415c1bf9749c0d654c853736783fb0d",
+    "url": "/mobile-store/static/media/apple-iphone-11-black.8415c1bf.jpg"
+  },
+  {
+    "revision": "fd345725099dc1b350af3d2146b3c488",
+    "url": "/mobile-store/static/media/apple-iphone-11-pro-gold.fd345725.jpg"
+  },
+  {
+    "revision": "38eed72a678b18ade95224b4641a76ef",
+    "url": "/mobile-store/static/media/apple-iphone-11-pro-silver.38eed72a.jpg"
+  },
+  {
+    "revision": "d90b58b059cea055cedeb5393ad5cd06",
+    "url": "/mobile-store/static/media/apple-iphone-11-purple.d90b58b0.jpg"
+  },
+  {
+    "revision": "7b5b093732fecadc8cb5de54d51db1a9",
+    "url": "/mobile-store/static/media/apple-iphone-midnight-green.7b5b0937.jpg"
+  },
+  {
+    "revision": "d917d854a8997be5aca631f808beda4c",
+    "url": "/mobile-store/static/media/apple-iphone-x-grey.d917d854.jpg"
+  },
+  {
+    "revision": "77ef097c6c725ec392e007995e574156",
+    "url": "/mobile-store/static/media/apple-iphone-xs-max-gold.77ef097c.jpg"
+  },
+  {
+    "revision": "19c238e4e3c1c24c33ba1194a5d91b9d",
+    "url": "/mobile-store/static/media/iPhone-SE.19c238e4.jpg"
+  },
+  {
+    "revision": "c6c37f3fa7fb8bf0c00ce53a08e6ecf5",
+    "url": "/mobile-store/static/media/iPhone11.c6c37f3f.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/mobile-store/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/mobile-store/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/mobile-store/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/mobile-store/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "75798c27b361271298d53eefc633ec7d",
+    "url": "/mobile-store/static/media/undraw_empty_cart.75798c27.svg"
+  }
+]);
